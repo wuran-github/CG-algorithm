fe64bf8cec41bd19b8a1ee767df989e52863b9a6
ran wu <wuranvv@outlook.com>
2018-09-25 22:56:36 +0800

    add IPolygon

---
+++ Line-Algorithm/Line-Algorithm/IPolygon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Algorithm
+{
+    public interface IPolygon
+    {
+        IEnumerable<Point> GetPoints(IEnumerable<Point> points);
+    }
+}
---
+++ Line-Algorithm/Line-Algorithm/XScan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Algorithm
+{
+    class XScan : IPolygon
+    {
+        public IEnumerable<Point> GetPoints(IEnumerable<Point> points)
+        {
+            List<Point> pointList = null;
+            int maxY = points.Max(en => en.Y);//后续用非自带方法
+            int minY = points.Min(en => en.Y);
+
+            return pointList;
+        }
+    }
+}
